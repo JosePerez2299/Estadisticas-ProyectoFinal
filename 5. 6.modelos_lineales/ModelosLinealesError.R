5ba9712053cd94190f10eaf4beae26c7df837da0
Jose Perez <16-10882@usb.ve>
2025-03-31 15:07:23 -0400

    inciso 5-6 carlos

--- 5. 6.modelos_lineales/ModelosLinealesError.R
+++ 5. 6.modelos_lineales/ModelosLinealesError.R
@@ -10,56 +10,49 @@ x5 <- Datos_ProyectoP7$x5
 x7 <- Datos_ProyectoP7$x7
 x8 <- Datos_ProyectoP7$x8
 
-# Modelos de regresión simple
-modelo_yx3 <- lm(y ~ x3)
-summary(modelo_yx3)
-modelo_yx5 <- lm(y ~ x5)
-summary(modelo_yx5)
-modelo_yx7 <- lm(y ~ x7)
-summary(modelo_yx7)
-modelo_yx8 <- lm(y ~ x8)
-summary(modelo_yx8)
+#Modelo de regresion simple y,x3,x5,x7,x8
 
-# Modelo lineal múltiple
-modelo1 <- lm(y ~ x3 + x5 + x7 + x8)
+modeloyx1 <- lm(y~x1)
+summary(modeloyx1)
+modeloyx2 <- lm(y~x2)
+summary(modeloyx2)
+modeloyx3 <- lm(y~x3)
+summary(modeloyx3)
+modeloyx4 <- lm(y~x4)
+summary(modeloyx4)
+modeloyx5 <- lm(y~x5)
+summary(modeloyx5)
+modeloyx7 <- lm(y~x7)
+summary(modeloyx7) #el mejor modelo de acuerdo al R^2 
+modeloyx8 <- lm(y~x8)
+summary(modeloyx8)
+
+#Modelo lineal multiple y,x1,x2,x3,x4,x5,x7,x8
+
+modelo1 <-  lm(y ~ x1+x2+x3+x4+x5+x7+x8)
 summary(modelo1)
 
-# Eliminación de variables
-modelo2 <- lm(y ~ x5 + x7 + x8)
+#Elimino x3
+
+modelo2 <-  lm(y ~ x1+x2+x4+x5+x7+x8)
 summary(modelo2)
-modelo3 <- lm(y ~ x5 + x7)
-summary(modelo3)  # Modelo aceptado
 
-# Modelo final y análisis de errores
-modelo_final <- modelo3
-residuos  <- resid(modelo_final)
-ajustados <- fitted(modelo_final)
+#Elimino x1
 
-# Usar el directorio actual para guardar gráficos
-img_dir <- "./imagenes"
+modelo3 <-  lm(y ~ x2+x4+x5+x7+x8)
+summary(modelo3)
 
-# Gráfico de residuos vs valores ajustados
-png(filename = file.path(img_dir, "5-6_residuos_vs_valores_ajustados.png"), width = 800, height = 600)
-plot(ajustados, residuos, main = "Residuos vs Valores Ajustados", 
-     xlab = "Valores Ajustados", ylab = "Residuos", pch = 19, col = "blue")
-abline(h = 0, col = "red", lwd = 2)
-dev.off()
+#Elimino x4
 
-# Histograma de residuos
-png(filename = file.path(img_dir, "5-6_histograma_residuos.png"), width = 800, height = 600)
-hist(residuos, main = "Histograma de Residuos", xlab = "Residuos", 
-     col = "lightgreen", border = "black")
-dev.off()
+modelo4 <-  lm(y ~ x2+x5+x7+x8)
+summary(modelo4)
 
-# QQ-plot de residuos
-png(filename = file.path(img_dir, "5-6_qqplot_residuos.png"), width = 800, height = 600)
-qqnorm(residuos, main = "QQ-Plot de Residuos", pch = 19, col = "darkblue")
-qqline(residuos, col = "red", lwd = 2)
-dev.off()
+#Elimino x5
 
-# Guardar los 4 gráficos de diagnóstico del modelo final
-png(filename = file.path(img_dir, "5-6_diagnosticos_modelo_final.png"), width = 800, height = 800)
-par(mfrow = c(2, 2))
-plot(modelo_final)
-dev.off()
-print(summary(modelo_final))
+modelo5 <-  lm(y ~ x2+x7+x8)
+summary(modelo5)
+
+#Elimino x8
+
+modelo6 <-  lm(y ~ x2+x7)
+summary(modelo6)
